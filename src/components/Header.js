9ab70362202490e30f7676b7f458ed013a487fd9
krishna das <dask2207@gmail.com>
2024-04-05 15:27:15 +0530

    change the bg of sign in page and created a login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Header = () => {
     return (
         <>
-            <div className='container m-auto'>
-                <img className='w-56 m-2' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo' />
+            <div className='bg-gradient-to-b from-black z-10 absolute w-full'>
+                <div className='container m-auto relative  '>
+                    <img className='w-52  mx-12 ' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo' />
+                </div>
             </div>
+            
         </>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,23 @@ const Login = () => {
     return (
         <>
             <Header />
+            <div className=''>
+                
+                <img   src='https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='body background' />
+            </div>
+
+            <div className='w-full h-[125%] bg-gradient-to-t from-black absolute top-0'>
+
+            </div>
+
+            <form className='bg-zinc-950 py-10 px-20 w-[500px] opacity-85 rounded-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40'>
+                <h2 className=' text-start text-white text-4xl py-4 font-bold'>Sign In</h2>
+                <input className='w-full text-lg p-4 rounded-md border-slate-50 border my-6 bg-gray-900' type='email' placeholder='Email or Phone Number' />
+                <input className='w-full text-lg rounded-md border-slate-50 border p-4 bg-gray-900' type='password' placeholder='Password' />
+                <button className='w-full h-12  rounded-md text-white font-bold text-lg my-6 bg-red-600'>Sign In</button>
+                <h2 className=' text-center text-white text-xl '>OR</h2>
+                <button className='w-full h-12  rounded-md text-white font-bold text-lg my-4 bg-stone-600'>Use a Sign-In Code</button>
+            </form>
         </>
     )
 }
