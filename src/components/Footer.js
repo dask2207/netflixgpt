c838f43a4acf679702076a936da8948eb36b6658
krishna das <dask2207@gmail.com>
2024-04-09 21:13:57 +0530

    signup form

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="absolute pt-10 m-10 text-white text-4xl bottom-1">Footer</div>
+  )
+}
+
+export default Footer
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        ttuPattern: "url('https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='body background')",
+      }
+    },
   },
   plugins: [],
 }
